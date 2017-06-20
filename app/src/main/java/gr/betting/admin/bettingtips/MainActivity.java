9f6de72fd6392d442cfb5acc73fba5db38f840b8
agelogeo (PC) <agelo1995@gmail.com>
2017-06-21 00:23:04 +0300

    Share button working Just once / always not working.

--- app/src/main/java/gr/betting/admin/bettingtips/MainActivity.java
+++ app/src/main/java/gr/betting/admin/bettingtips/MainActivity.java
@@ -1,17 +1,17 @@
 package gr.betting.admin.bettingtips;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,8 +20,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    FragmentManager mFragmentManager;
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +72,22 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+
+        // Return true to display menu
         return true;
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
     }
 
     @Override
@@ -83,9 +98,15 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        /*if (id == R.id.action_settings) {
+        if (id == R.id.menu_item_share) {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.     https://www.youtube.com/watch?v=HZ3UWScq_0Y");
+            sendIntent.setType("text/plain");
+            Intent.createChooser(sendIntent,"Share via");
+            startActivity(sendIntent);
             return true;
-        }*/
+        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -113,7 +134,11 @@ public class MainActivity extends AppCompatActivity
             transaction.commit();
         } else if (id == R.id.nav_feedback) {
             Toast.makeText(this,"This function is not implemented yet.", Toast.LENGTH_SHORT).show();
-
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.     https://www.youtube.com/watch?v=HZ3UWScq_0Y");
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
 
         } else if (id == R.id.nav_settings) {
             Toast.makeText(this,"This function is not implemented yet.", Toast.LENGTH_SHORT).show();
