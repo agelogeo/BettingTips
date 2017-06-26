f55bae2b149eb5958b3f4f23fe9f3276183a7b9e
agelogeo (PC) <agelo1995@gmail.com>
2017-06-27 02:48:23 +0300

    About activity changed and updated. Feedback feature configured. A custom rate dialog added. gr/en

--- app/src/main/java/gr/betting/admin/bettingtips/MainActivity.java
+++ app/src/main/java/gr/betting/admin/bettingtips/MainActivity.java
@@ -1,8 +1,13 @@
 package gr.betting.admin.bettingtips;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
@@ -46,7 +51,7 @@ public class MainActivity extends AppCompatActivity
 
         MobileAds.initialize(this, getString(R.string.app_ad_unit_id));
 
-
+        AskRating();
 
 
 
@@ -202,8 +207,17 @@ public class MainActivity extends AppCompatActivity
             //transaction.addToBackStack(null);
             transaction.commit();
         } else if (id == R.id.nav_feedback) {
-            Toast.makeText(this,"This function is not implemented yet.", Toast.LENGTH_SHORT).show();
-
+            final String appName = getApplicationContext().getPackageName();
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id="
+                                + appName)));
+            } catch (android.content.ActivityNotFoundException anfe) {
+                startActivity(new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id="
+                                + appName)));
+            }
         } else if (id == R.id.nav_bonus_tips) {
             fragment = new BonusFragment();
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -219,6 +233,7 @@ public class MainActivity extends AppCompatActivity
                 Log.e(LOG_TAG, "Share action provider is null?");
             }
         }   else if (id == R.id.nav_info) {
+            navigationView.setCheckedItem(R.id.nav_info);
             fragment = new InfoFragment();
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             transaction.replace(R.id.mainFrame,fragment);
@@ -269,5 +284,70 @@ public class MainActivity extends AppCompatActivity
 
         // The directory is now empty so delete it
         return dir.delete();
+    }
+
+    public void AskRating(){
+        // Count times app has been opened, display rating message after number of times
+        // By Rafael Duval
+        try {
+
+            // Get the app's shared preferences
+            SharedPreferences app_preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+            // Get the value for the run counter
+            int counter = app_preferences.getInt("counter_starts", 0);
+
+            // Do every x times
+            //int RunEvery = 20;
+
+            if(counter != 0  && counter == 5 || counter == 20 || counter == 50 || counter == 150 || counter == 300 )
+            {
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle(R.string.we_need_your_help);
+                alert.setIcon(R.drawable.ic_loyalty_black_24dp); //app icon here
+                alert.setMessage(R.string.please_take_a_moment);
+
+                alert.setPositiveButton(R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int whichButton) {
+                                //Do nothing
+                            }
+                        });
+
+                alert.setNegativeButton(R.string.rate_it,
+                        new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                final String appName = getApplicationContext().getPackageName();
+                                try {
+                                    startActivity(new Intent(Intent.ACTION_VIEW,
+                                            Uri.parse("market://details?id="
+                                                    + appName)));
+                                } catch (android.content.ActivityNotFoundException anfe) {
+                                    startActivity(new Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse("http://play.google.com/store/apps/details?id="
+                                                    + appName)));
+                                }
+
+                            }
+                        });
+                alert.show();
+            }
+
+            Toast.makeText(this, "This app has been started " + counter + " times.", Toast.LENGTH_SHORT).show();
+
+
+            // Increment the counter
+            SharedPreferences.Editor editor = app_preferences.edit();
+            editor.putInt("counter_starts", ++counter);
+            editor.commit(); // Very important
+
+        } catch (Exception e) {
+            //Do nothing, don't run but don't break
+        }
     }
 }
