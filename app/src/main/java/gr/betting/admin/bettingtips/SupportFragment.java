bd7dfcc5ec2b932088e213d141217d0d870ed65e
Giorgos Angelopoulos <agelo1995@gmail.com>
2017-08-15 16:20:36 +0300

    layout created.

--- app/src/main/java/gr/betting/admin/bettingtips/SupportFragment.java
+++ app/src/main/java/gr/betting/admin/bettingtips/SupportFragment.java
@@ -36,20 +36,14 @@ public class SupportFragment extends Fragment {
     String license_key_two = "X4Wq6Hkz3K0leG5R30/m9eS8IeGUbi/BmtHSP67GaLXetKrkKiQOE4NwuEqI9ooxaspEMgcRuJ0R+w2YfXZQrCITFB4AbS68vqf24dayQFqfd37OrMzkEEihVpdQvfH7Yz";
     String license_key_three = "+IeTfKSCfu/KfYo93N2BOMmsyXS1MAg1rg/ZOA1A4x/dF3rQr03hZWm9VnnTt82KTgx61HoJzBdx7dZ9YasxwrA1zGSAQIDAQAB";
     private static final String TAG = "bettingtips";
-    private Button clickButton;
-    private Button testButton,friendlyButton,goldButton,vipButton,subButton;
+    private Button friendlyButton,goldButton,vipButton,subButton;
 
-
-    IabHelper mHelper;
-    static final String ITEM_SKU = "com.example.vip_donation";
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
             View v = inflater.inflate(R.layout.support_layout, null);
 
-            testButton = (Button)v.findViewById(R.id.testButton);
             friendlyButton = (Button)v.findViewById(R.id.friendlyButton);
-            goldButton = (Button)v.findViewById(R.id.goldButton);
             vipButton = (Button)v.findViewById(R.id.vipButton);
             subButton = (Button)v.findViewById(R.id.subButton);
 
@@ -73,25 +67,7 @@ public class SupportFragment extends Fragment {
                 }
             });
 
-            goldButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Bundle buyIntentBundle = null;
-                    try {
-                        buyIntentBundle = CallHolder.getmService().getBuyIntent(3,getActivity().getPackageName(),
-                                "gold_donation", "inapp", "");
-                        PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
 
-                        getActivity().startIntentSenderForResult(pendingIntent.getIntentSender(),
-                                1001, new Intent(), Integer.valueOf(0), Integer.valueOf(0),
-                                Integer.valueOf(0));
-                    } catch (RemoteException e) {
-                        e.printStackTrace();
-                    } catch (IntentSender.SendIntentException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
 
             vipButton.setOnClickListener(new View.OnClickListener() {
                 @Override
